fe66eb15703f5be56080646ff805c00d23c95818
Sumin13 <su184913@icloud.com>
2024-02-15 23:29:07 +0000

    15.02.24

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ function toggleBlink() {
   element.classList.toggle("blinking");
 }
 setInterval(toggleBlink, 1000);
+
+function showWelcomeMessage() {
+  var name = document.getElementById("name").value;
+  if (name.trim() !== "") {
+    document.getElementById("nameInput").style.display = "none";
+    document.getElementById("welcomeHeader").innerText = "WELCOME, " + name;
+    document.getElementById("welcomeMessage").style.display = "block";
+  } else {
+    alert("Please write your name");
+  }
+}
+function goToNextPage() {
+  var count = 3;
+  var countdownInterval = setInterval(function () {
+    if (count > 0) {
+      document.getElementById("welcomeHeader").style.display = "none";
+      document.querySelector("#welcomeMessage button").style.display = "none";
+
+      document.querySelector(".countdown").innerText = count;
+
+      count--;
+      document.querySelector("#welcomeMessage button").display = none;
+    } else {
+      clearInterval(countdownInterval);
+      window.location.href = "main.html";
+    }
+  }, 1000);
+}
